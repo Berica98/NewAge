09e582dc9148e084044c04d641870519fab46a21
marcelinus <marcelinusilonuba21@gmail.com>
2024-12-30 14:08:38 +0100

    add announcement endpoint

--- Backend/announcement/url.py
+++ Backend/announcement/url.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.home, name='home'),
   
     #path('create/', views.create_annoucement, name='create_announcement'),
-    path('<int:class_id>/', views.get_announcement, name='get_announcement')
+    path('api/v1/announcement/', views.get_all_announcement, name='get_all_announcement'),
+    path('api/v1/announcement/<int:class_id>/', views.get_announcement, name='get_announcement')
 ]
