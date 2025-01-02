bbebfe2b8492d8b6c5f233c60982865062b9f3d5
marcelinus <marcelinusilonuba21@gmail.com>
2025-01-02 18:41:00 +0100

    impliment login api

--- Backend/registration/url.py
+++ Backend/registration/url.py
@@ -5,4 +5,5 @@ from django.http import JsonResponse
 urlpatterns = [
     path('api/v1/students/', views.get_all_students, name='get_all_students'),
     #path('api/v1/announcement/<int:class_id>/', views.get_announcement, name='get_announcement')
+    path('api/v1/students/login/', views.login_view, name='login_view')
 ]
