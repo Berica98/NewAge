632f9882d242048210a83e25647ff3100b19ab4a
marcelinus <marcelinusilonuba21@gmail.com>
2025-01-05 00:24:50 +0100

    teachers modefile url

--- Backend/teachers/urls.py
+++ Backend/teachers/urls.py
@@ -1,11 +1,11 @@
 #from django.conf.urls import url
-from django.urls import path# re_path
+from django.urls import path, re_path
 #from toys import views 
 from . import views
 
 urlpatterns = [
 	#url(r'^teachers/$', views.teachers_list),
 	#url(r'^teachers/(?P<pk>[0-9]+)$', views.teachers_detail),
-    #re_path(r'^teachers/(?P<pk>[0-9]+)$', views.teachers_detail),
-    path('api/v1/teachers/', views.Teachers, name='Teachers'),
+    re_path(r'^teachers/(?P<pk>[0-9]+)$', views.teacher_detail),
+    path('api/v1/teachers/', views.teacher_list, name='teacher_list'),
 ]
