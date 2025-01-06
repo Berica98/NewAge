91bdd3b18d1b143b31893bacfbf33f12e5fe3f44
Berica98 <berica.george@gmail.com>
2025-01-06 12:39:03 +0100

    jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // Include your global CSS styles
+import './index.css';
 
-// Render the React app to the DOM
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
